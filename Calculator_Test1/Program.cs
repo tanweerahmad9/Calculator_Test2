09dfdd1f15770f9b5ef554edef874dfb2c985342
tanweerahmad9 <140061768+tanweerahmad9@users.noreply.github.com>
2023-07-26 10:20:04 +0530

    #Change in code line of 41 have removed the division part.

--- Calculator_Test1/Program.cs
+++ Calculator_Test1/Program.cs
@@ -36,10 +36,6 @@ namespace calculator_c_sharp
                         res = num1 * num2;
                         Console.WriteLine("Multiplication:" + res);
                         break;
-                    case "/":
-                        res = num1 / num2;
-                        Console.WriteLine("Division:" + res);
-                        break;
                     default:
                         Console.WriteLine("Wrong input");
                         break;
